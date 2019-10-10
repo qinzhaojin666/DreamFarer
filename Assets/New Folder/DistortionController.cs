caba6f655a2a2cc6adddf91e109faa7ae4a4cc95
esargent <esargent@andrew.cmu.edu>
2019-10-09 21:17:58 -0400

    Test interaction for custom setting of parameters
    Played around with FMOD/Unity to have a custom parameter. There is about 14 seconds of traffic noise, and the custom parameter "x" ranges from 0-25, with custom distortion automation spread across the whole thing. The parameter changes as time progresses. You can press K to stop the automation and have the sound progress to its end without the automation, or L to kill the sound

---
+++ Assets/New Folder/DistortionController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+using FMOD;
+
+public class DistortionController : MonoBehaviour
+{
+    [FMODUnity.EventRef] public string trafficPath = "event:/Traffic";
+    public FMOD.Studio.EventInstance trafficInstance;
+    private bool usable = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        trafficInstance = RuntimeManager.CreateInstance(trafficPath);
+        trafficInstance.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(gameObject));
+        trafficInstance.start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K) && usable) {
+            trafficInstance.release();
+            usable = false;
+        }
+        else if (Input.GetKeyDown(KeyCode.L)) {
+            trafficInstance.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+        }
+
+        if (usable) {
+            float currentTrafficVal;
+            trafficInstance.getParameterByName("x", out currentTrafficVal);
+            float newTrafficVal = currentTrafficVal + 0.05f;
+            while (newTrafficVal >= 25.0f) {
+                newTrafficVal -= 25.0f;
+            }
+            trafficInstance.setParameterByName("x", newTrafficVal);
+        }
+    }
+}
