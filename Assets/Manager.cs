75ae4b9467db803087a66712c6e7990735775c02
Vidhart Bhatia <vidhartbhatia@hotmail.com>
2019-11-04 19:50:17 -0500

    added scene transition on grab image

---
+++ Assets/Manager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+    public GameObject alleyObjects;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(test());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+   public void startDisableAlleyObjects()
+    {
+        StartCoroutine(disableAlleyObjects());
+    }
+
+    IEnumerator disableAlleyObjects()
+    {
+        foreach (Transform child in alleyObjects.transform)
+        {
+            yield return new WaitForSeconds(1);
+            if (child.gameObject.CompareTag("popup"))
+            {
+                print("popup found");
+                float count = 0;
+                while (count <= 90)
+                {
+                    child.Rotate(new Vector3(0, 0, -1f));
+                    //child.RotateAround( , new Vector3(-1f, 0, 0));
+                    count += 1;
+                    yield return new WaitForSeconds(.00001f);
+                }
+            }
+            child.gameObject.SetActive(false);
+        }
+    }
+
+   
+   
+
+    IEnumerator test()
+    {
+        yield return new WaitForSeconds(5);
+        StartCoroutine(disableAlleyObjects());
+
+    }
+}
---
+++ Assets/OnGrabImage.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnGrabImage : MonoBehaviour
+{
+    public Manager manger;
+    private bool grabbedOnce = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool isGrabbed = GetComponent<OVRGrabbable>().isGrabbed;
+        if ( !grabbedOnce && isGrabbed)
+        {
+            grabbedOnce = true;
+            whenGrabbed();
+        }
+    }
+
+    void whenGrabbed()
+    {
+        manger.startDisableAlleyObjects();
+    }
+}
