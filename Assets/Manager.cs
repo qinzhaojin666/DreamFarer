c5d93b67c77f8562a71565977f30cefe70ff6bde
Vidhart Bhatia <vidhartbhatia@hotmail.com>
2019-11-07 11:54:53 -0500

    started on bar

--- Assets/Manager.cs
+++ Assets/Manager.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Manager : MonoBehaviour
 {
     public GameObject alleyObjects;
+    public GameObject barObjects;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,11 +24,17 @@ public class Manager : MonoBehaviour
         StartCoroutine(disableAlleyObjects());
     }
 
+    public void startEnableBarObjects()
+    {
+        StartCoroutine(enableBarObjects());
+
+    }
+
     IEnumerator disableAlleyObjects()
     {
         foreach (Transform child in alleyObjects.transform)
         {
-            yield return new WaitForSeconds(.3f);
+            yield return new WaitForSeconds(.1f);
             if (child.gameObject.CompareTag("popup"))
             {
                 print("popup found");
@@ -41,10 +49,32 @@ public class Manager : MonoBehaviour
             }
             child.gameObject.SetActive(false);
         }
+        startEnableBarObjects();
     }
 
-   
-   
+    IEnumerator enableBarObjects()
+    {
+        foreach (Transform child in barObjects.transform)
+        {
+            yield return new WaitForSeconds(.1f);
+            //if (child.gameObject.CompareTag("popup"))
+            //{
+            //    print("popup found");
+            //    float count = 0;
+            //    while (count <= 45)
+            //    {
+            //        child.Rotate(new Vector3(0, 0, -2f));
+            //        //child.RotateAround( , new Vector3(-1f, 0, 0));
+            //        count += 2;
+            //        yield return new WaitForSeconds(.0001f);
+            //    }
+            //}
+            child.gameObject.SetActive(true);
+        }
+    }
+
+
+
 
     IEnumerator test()
     {
