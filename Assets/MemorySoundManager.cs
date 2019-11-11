5dbcf699b0e86d84929242697d439ef3420a40a2
esargent <esargent@andrew.cmu.edu>
2019-11-11 12:49:27 -0500

    Smoothened parameter changes
    Used Mathf.MoveTowards to smooth out parameter changes, based on a new delta variable

--- Assets/MemorySoundManager.cs
+++ Assets/MemorySoundManager.cs
@@ -10,6 +10,9 @@ public class MemorySoundManager : MonoBehaviour
     public GameObject playerCamera;
     public float maxDistance = 10;
 
+    // Delta = max change in parameter per frame (allows for a smooth transition)
+    public float delta = 0.075f;
+
     private FMOD.Studio.EventInstance memoryInstance;
     private readonly string grabbedDistName = "grabbedDistance";
 
@@ -38,7 +41,7 @@ public class MemorySoundManager : MonoBehaviour
 
     /*
      * Given a distance between the object and listener, returns the
-     *   corresponding parameter value. For now, returns a simple scaling
+     *   corresponding parameter value in [0, 1]. For now, returns a simple scaling
      *   based on maxDistance, but may be swapped out for something smoother.
      */
     float getParameterFromDistance(float distance) {
@@ -52,17 +55,18 @@ public class MemorySoundManager : MonoBehaviour
          * GrabbedDistance behavior:
          *  - The higher the grabbedDistance is, the more dulled the sound is, and the initial loop
          *    can only be broken out of with a grabbedDistance of 0.
-         *  - If the object is currently grabbed, the parameter is automatically set to 0.
+         *  - If the object is currently grabbed, the parameter decreases by delta each frame until it reaches 0.
          *  - Otherwise, the parameter is set to the distance between the object and the player listener.
          */
 
         float grabbedDistance;
+        memoryInstance.getParameterByName(grabbedDistName, out grabbedDistance);
         if (grabber.isGrabbed) {
-            grabbedDistance = 0;
+            grabbedDistance = Mathf.MoveTowards(grabbedDistance, 0.0f, delta);
         }
         else {
             float dist = Vector3.Distance(gameObject.transform.position, playerCamera.transform.position);
-            grabbedDistance = getParameterFromDistance(dist);
+            grabbedDistance = Mathf.MoveTowards(grabbedDistance, getParameterFromDistance(dist), delta);
         }
         memoryInstance.setParameterByName(grabbedDistName, grabbedDistance);
     }
